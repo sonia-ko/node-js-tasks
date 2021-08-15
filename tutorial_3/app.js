3489529cf6a0f1c6ee4cb7e7582a245090b43e71
Sonia Kozytska <sonya-ko@outlook.com>
2021-08-15 23:36:51 +0300

    edit the configuration for the task 3

--- tutorial_3/app.js
+++ tutorial_3/app.js
@@ -1,12 +1,12 @@
 const http = require("http");
 const env = require("dotenv").config();
-const processStaticFile = require('./processStaticFile')
+const processStaticFile = require("./processStaticFile");
 const { CONTENT_TYPES } = require("./config");
 const fs = require("fs");
-const restAPI = require('./restAPI')
+const restAPI = require("./restAPI");
 let pageNotFound;
 
-fs.readFile('./static/pageNotFound.html', function(err, page) {
+fs.readFile("./static/pageNotFound.html", function(err, page) {
     if (err) {
         throw err;
     }
@@ -20,7 +20,6 @@ http
             res.end();
         } else if (req.url.startsWith("/api/")) {
             restAPI(req, res);
-
         } else if (req.url.startsWith("/static/")) {
             processStaticFile(req, res);
         } else {
@@ -28,21 +27,8 @@ http
             res.write(pageNotFound);
             res.end();
         }
-
     })
-    //     if (req.url.startsWith("/static/")) {
-    //         processStaticFile(req, res);
-    //     } else if (req.url.startsWith("/api")) {
-    //         console.log('api');
-    //     } else if (req.url === "/") {
-    //         res.writeHead(302, { Location: "/static/index.html" });
-    //         res.end();
-    //     } else {
-    //         res.writeHead(404, { "Content-Type": "text/html" });
-    //         res.write(pageNotFound);
-    //         res.end();
-    //     }
-    // })
-    .listen(env.parsed.PORT, () =>
-        console.log(`Server started on: http://localhost:${env.parsed.PORT}`)
-    );
+
+.listen(env.parsed.PORT, () =>
+    console.log(`Server started on: http://localhost:${env.parsed.PORT}`)
+);
